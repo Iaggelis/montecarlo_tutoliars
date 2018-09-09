5a840bf9bdeffa8ea7ac17525ba468f23ee7d842
John Angelis <john_agelis@hotmail.com>
2018-09-09 11:56:22 +0300

    Refactored hepevt converter

--- HepEVT/converter.cpp
+++ HepEVT/converter.cpp
@@ -1,20 +1,13 @@
 /* 
 A simple script for converting hepevt format files to root files.
-This one keep only leptons,but the same logic can apply for any other implementation.
-
-
+Z + 1 jet events with Z decaying to leptons is used. Only particles with stable final states will be saved.
 */
+
 #include <iostream>
 #include <fstream>
-#include <cmath>
 #include <vector>
 #include <chrono>
-#include <algorithm>
-#include <iterator>
-#include <stdexcept>
-#include <sstream> // std::istringstream
-#include <string>  // std::string
-#include <iomanip>
+#include <string> // std::string
 #include <ROOT/TTree.h>
 #include <ROOT/TFile.h>
 #include <ROOT/TLorentzVector.h>
@@ -77,7 +70,7 @@ typedef struct
 
 int main(int argc, char *argv[])
 {
-    auto begin = std::chrono::steady_clock::now();
+    auto begin = chrono::steady_clock::now();
 
     // reading the .hepevt file
     ifstream inputFile;
@@ -85,25 +78,23 @@ int main(int argc, char *argv[])
 
     // creating the root file
     TFile *outputfile = TFile::Open("example.root", "recreate");
-    vector<int> lep_ids;
-    vector<double> lep_px;
-    vector<double> lep_py;
-    vector<double> lep_pz;
-    vector<double> lep_e;
-    int n_OfLeptons;
-    TTree *leptonsTree = new TTree("leptons", "Z+3jets  leptons");
-    leptonsTree->Branch("n_OfLeptons", &n_OfLeptons);
-    leptonsTree->Branch("lep_ids", &lep_ids);
-    leptonsTree->Branch("lep_px", &lep_px);
-    leptonsTree->Branch("lep_py", &lep_py);
-    leptonsTree->Branch("lep_pz", &lep_pz);
-    leptonsTree->Branch("lep_e", &lep_e);
+    // declaring the vectors used for storing the particle id and kinematic variables
+    vector<int> par_ids;
+    vector<double> par_px, par_py, par_pz, par_e;
+    int n_particles;
+    TTree *leptonsTree = new TTree("particles", "Z+1jets  particles");
+    leptonsTree->Branch("n_particles", &n_particles);
+    leptonsTree->Branch("par_ids", &par_ids);
+    leptonsTree->Branch("par_px", &par_px);
+    leptonsTree->Branch("par_py", &par_py);
+    leptonsTree->Branch("par_pz", &par_pz);
+    leptonsTree->Branch("par_e", &par_e);
 
     int NEVHEP; // Event number
     int NHEP;   // Number of particles in the event
     while (inputFile.good())
     {
-        int lepton_counter = 0;
+        int particle_counter = 0;
         inputFile >> NEVHEP >> NHEP;
         if (inputFile.eof())
         {
@@ -113,34 +104,37 @@ int main(int argc, char *argv[])
         myHEPEVT event(NHEP);
         for (int n = 0; n < NHEP; n++)
         {
+            // Using the struct to store the event
+            // inputFile >> event.nparhep[n] >> event.isthep[n] >> event.idhep[n] >> event.jmohep[n][0] >> event.jmohep[n][1] >>
+            //     event.jdahep[n][0] >> event.jdahep[n][1] >> event.phep[n][0] >> event.phep[n][1] >> event.phep[n][2] >> event.phep[n][3] >> event.phep[n][4] >>
+            //     event.vhep[n][0] >> event.vhep[n][1] >> event.vhep[n][2] >> event.vhep[n][3];
 
-            // inputFile >> event.nparhep[n] >> event.isthep[n] >> event.idhep[n] >> event.jmohep[n][0] >> event.jmohep[n][1] >> event.jdahep[n][0] >> event.jdahep[n][1] >> event.phep[n][0] >> event.phep[n][1] >> event.phep[n][2] >> event.phep[n][3] >> event.phep[n][4] >> event.vhep[n][0] >> event.vhep[n][1] >> event.vhep[n][2] >> event.vhep[n][3];
-
+            // Using the class to store the event
             inputFile >> event.nparhep[n] >> event.isthep[n] >> event.idhep[n] >> event.jmohep[n].first >> event.jmohep[n].second >>
                 event.jdahep[n].first >> event.jdahep[n].second >>
                 event.phep[n].px >> event.phep[n].py >> event.phep[n].pz >> event.phep[n].e >> event.phep[n].mass >>
                 event.vhep[n].xx >> event.vhep[n].yy >> event.vhep[n].zz >> event.vhep[n].prod_time;
 
             // status code of 1 translates to stable final state
-            if ((event.isthep[n] == 1) && (abs(event.idhep[n]) == 11 || abs(event.idhep[n]) == 13))
+            if (event.isthep[n] == 1)
             {
-                lepton_counter++;
-                lep_ids.push_back(event.idhep[n]);
-                lep_px.push_back(event.phep[n].px);
-                lep_py.push_back(event.phep[n].py);
-                lep_pz.push_back(event.phep[n].pz);
-                lep_e.push_back(event.phep[n].e);
+                particle_counter++;
+                par_ids.push_back(event.idhep[n]);
+                par_px.push_back(event.phep[n].px);
+                par_py.push_back(event.phep[n].py);
+                par_pz.push_back(event.phep[n].pz);
+                par_e.push_back(event.phep[n].e);
             }
         }
 
-        n_OfLeptons = lepton_counter;
+        n_particles = particle_counter;
         leptonsTree->Fill();
 
-        lep_ids.clear();
-        lep_px.clear();
-        lep_py.clear();
-        lep_pz.clear();
-        lep_e.clear();
+        par_ids.clear();
+        par_px.clear();
+        par_py.clear();
+        par_pz.clear();
+        par_e.clear();
     }
 
     inputFile.close();
@@ -148,7 +142,7 @@ int main(int argc, char *argv[])
     outputfile->Write();
     outputfile->Close();
 
-    auto end = std::chrono::steady_clock::now();
+    auto end = chrono::steady_clock::now();
 
     cout << "Running time: " << chrono::duration_cast<chrono::minutes>(end - begin).count() << endl;
     return 0;
